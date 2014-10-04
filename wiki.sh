240646c7779339a9e1ea0a68bc930f8bdb35635a
equialgo <equialgo@gmail.com>
2014-10-04 18:25:13 +0200

    fixing bus in wiki launch script

--- wiki.sh
+++ wiki.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 (sleep 5; python -mwebbrowser http://localhost:5000/localfile/Main%20Page.ipynb) &
-vagrant ssh -c "python -m nbviewer --no-cache --localfiles='/home/vagrant/Projects/Wiki'" 
+vagrant ssh -c "cd /home/vagrant/.nbviewer && python -m nbviewer --no-cache --localfiles='/home/vagrant/Projects/Wiki'" 
