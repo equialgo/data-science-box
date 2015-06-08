83648534926cc8e518707c0f33a1dd2446e1b903
equialgo <equialgo@gmail.com>
2015-06-08 07:59:27 +0200

    updating spark to version 1.3.1

--- ipyspnb_server.sh
+++ ipyspnb_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 (sleep 5; python -mwebbrowser http://localhost:8888) &
-vagrant ssh -c "IPYTHON_OPTS='notebook --ip=*' /usr/local/spark-*-bin-hadoop2.4/bin/pyspark" 
+vagrant ssh -c "IPYTHON_OPTS='notebook --ip=*' /usr/local/spark-*-bin-*/bin/pyspark" 
