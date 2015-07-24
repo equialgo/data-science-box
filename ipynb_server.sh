92db9ae933fd84d46e3e390d0ca68516de3748db
equialgo <equialgo@gmail.com>
2015-07-24 10:58:27 +0200

    Update ipynb_server.sh

--- ipynb_server.sh
+++ ipynb_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-(sleep 5; python -mwebbrowser http://localhost:8888) &
-vagrant ssh -c "ipython notebook --profile dsb" 
+input=$1
+if [ "$input" = "stop" ] ; then
+    vagrant ssh -c "source .profile; stop_ipynb_server"
+else
+    (sleep 5; python -mwebbrowser http://localhost:8888) &
+    vagrant ssh -c "source .profile; start_ipynb_server"
+fi
