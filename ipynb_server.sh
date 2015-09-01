261f0c469b2e6891be4ce874db6f499ecd8d5f6b
equialgo <equialgo@gmail.com>
2015-09-01 15:48:45 +0200

    removing profiles

--- ipynb_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-input=$1
-if [ "$input" = "stop" ] ; then
-    vagrant ssh -c "source .profile; stop_ipynb_server"
-else
-    (sleep 5; python -mwebbrowser http://localhost:8888) &
-    vagrant ssh -c "source .profile; start_ipynb_server"
-fi
--- ipysh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-vagrant ssh -c "ipython --profile sh" 
